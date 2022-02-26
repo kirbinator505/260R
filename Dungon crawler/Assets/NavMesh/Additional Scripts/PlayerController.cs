3be8baaf5ccca64cca8be5b9542150c5873b06da
Ethan <10841219@my.uvu.edu>
2022-02-25 17:52:03 -0700

    began working with navmesh

---
+++ Dungon crawler/Assets/NavMesh/Additional Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // move out agent (Will make this the destination ball in the grid movement thing)
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
