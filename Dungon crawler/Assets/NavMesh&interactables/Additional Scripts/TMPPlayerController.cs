2f4bbf72b4d45b2e3bb56ac6c570fcb1dafed3ab
Ethan <10841219@my.uvu.edu>
2022-03-24 23:29:00 -0600

    making a new system of interaction

---
+++ Dungon crawler/Assets/NavMesh&interactables/Additional Scripts/TMPPlayerController.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TMPPlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    private Ray ray;
+    private RaycastHit hit;
+    public Vector3SO playerPos;
+    private Interactable interactable, focus;
+    private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // move out agent (Will make this the destination ball in the grid movement thing)
+                agent.SetDestination(hit.point);
+                RemoveFocus();
+            }
+        }
+        if (Input.GetMouseButtonDown(1))// this uses right click, I'm going to have to find some way to convert this to touch
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+        playerPos.pos = transform.position;
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDefocused();
+            focus = newFocus;
+            FollowTarget(newFocus);
+        }
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if(focus != null)
+            focus.OnDefocused();
+        focus = null;
+        StopFollowingTarget();
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.interactionRadius * 0.8f;
+        agent.updateRotation = false;
+        
+        target = newTarget.interactionTransform;
+    }
+
+    public void StopFollowingTarget()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+
+            target = null;
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime *5f);
+    }
+}
---
+++ Dungon crawler/Assets/NavMesh&interactables/interactables/Interactable.cs
@@ -0,0 +1,52 @@
+
+using System;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float interactionRadius = 3f;
+
+    private bool isFocus = false, hasInteracted = false;
+    private Transform player;
+    public Transform interactionTransform;
+    private float distance;
+
+    public virtual void Interact()
+    {
+        //this method is meant to be overwritten
+        Debug.Log("interacting with " + transform.name);
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= interactionRadius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, interactionRadius);
+    }
+}
