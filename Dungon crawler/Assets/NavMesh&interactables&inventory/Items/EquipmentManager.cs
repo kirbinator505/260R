76682267a2ce2045e0c4ed00332089473fcd1c96
Ethan <10841219@my.uvu.edu>
2022-03-31 18:00:31 -0600

    got equipment started

---
+++ Dungon crawler/Assets/NavMesh&interactables&inventory/Items/EquipmentManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+    #region Singleton
+    
+    public static EquipmentManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    
+    #endregion 
+    //need to replace the singletons, seems pretty easy just to change them into scriptable objects
+    private Equipment[] currentEquipment;
+
+    private Inventory inventory;
+
+    public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
+    public OnEquipmentChanged onEquipmentChanged;
+
+    private void Start()
+    {
+        inventory = Inventory.instance;
+        
+        int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numSlots];
+    }
+
+    public void Equip(Equipment newItem)
+    {
+        int slotIndex = (int) newItem.equipSlot;
+
+        Equipment oldItem = null;
+
+        if (currentEquipment[slotIndex] != null)
+        {
+            oldItem = currentEquipment[slotIndex];
+            inventory.AddItem(oldItem);
+        }
+
+        if (onEquipmentChanged != null)
+        {
+            onEquipmentChanged.Invoke(newItem, oldItem);
+        }
+
+        currentEquipment[slotIndex] = newItem;
+    }
+
+    public void Unequip(int slotIndex)
+    {
+        if (currentEquipment[slotIndex] != null)
+        {
+            Equipment oldItem = currentEquipment[slotIndex];
+            inventory.AddItem(oldItem);
+            currentEquipment[slotIndex] = null;
+            
+            if (onEquipmentChanged != null)
+            {
+                onEquipmentChanged.Invoke(null, oldItem);
+            }
+        }
+    }
+
+    public void UnequipAll()
+    {
+        for (int i = 0; i < currentEquipment.Length; i++)
+        {
+            Unequip(i);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))// will probably replace this call with a ui element as mobile doesn't have a keyboard
+        {
+            UnequipAll();
+        }
+    }
+}
